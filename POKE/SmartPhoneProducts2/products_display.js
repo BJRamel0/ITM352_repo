cf69f59b330aa8ce1a9e897859a9ead4fb421af3
BJRamel <Bramel@hawaii.edu>
2023-10-08 18:03:20 -1000

    xxx

--- POKE/SmartPhoneProducts2/products_display.js
+++ POKE/SmartPhoneProducts2/products_display.js
@@ -1,4 +1,6 @@
 //initialize variables
+const first_name="BJ";
+const last_name="Ramel";
 const store_name="BJ";
 let hits=0;
 let spins=0;
@@ -9,7 +11,64 @@ spins_span.innerHTML = spins;
 
 //declare and push to the DOM the store name at top and bottom
 top_title.innerHTML=(store_name + "'s Used Smart Phone Store");
-bottom_title.innerHTML=(store_name + "'s Used Smart Phone Store");
+
+//POKE8 Defining variables (name, price, image)
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+//Looping HTML
+for (let i=1; eval("typeof name"+i)!='undefined'; i++) {
+    document.querySelector('.main').innerHTML += `
+    <section class="item" onmouseover="changeClassName(this);" onclick="resetClassName(this);">
+        <h2>${eval("name"+i)}</h2>
+        <p>$${eval("price"+i)}</p>
+        <img src="${eval("image"+i)}">
+    </section>`;
+}
+
+const currentYear=new Date().getFullYear();
+const currentTime=new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
+
+
+const footerTable = `
+    <table>
+        <tr>
+            <td></td>
+            <td class="table-header">Your One Stop for Used Phones - ${first_name.charAt(0).toUpperCase()}.${last_name.charAt(0).toUpperCase()}'s</td>
+        </tr>
+        <tr>
+            <td>1.</td>
+            <td>Copyright @ ${first_name} ${last_name}</td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>${currentYear}</td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>${currentTime}</td>
+        </tr>
+    </table>
+`;
+bottom_title.innerHTML=footerTable;
+
 
 //Declaring Functions---
 //Spins Function
@@ -19,16 +78,11 @@ function changeClassName(element) {
         element.className='item rotate';
         spins=spins+1;
     }
-    //spins=spins+1; 
 
     //False, or else, True Win
     if(spins<2*hits&&hits<spins){
-        //wins=true;
         over_half=true;
-    } else {
-        //wins=false;
     }
-    //win_span.innerHTML=wins;
     win_span.innerHTML=over_half;
     spins_span.innerHTML = spins; 
     hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
@@ -36,32 +90,6 @@ function changeClassName(element) {
     // -- Winning progress depends on hits/spins
     let progress;
     let hits_spins_ratio = hits/spins;
-    /*if ( hits_spins_ratio > 0 ) {
-        progress = 'On your way!';
-        if ( hits_spins_ratio >= 0.25 ) {
-            progress = 'Almost there!';
-            if ( hits_spins_ratio >= 0.5 ) {
-                if( hits < spins) { 
-                    progress = 'You win!';
-                }
-            }
-        }
-    } else {
-        progress = 'Get going!' ;
-    }*/
-    
-    //Revamped hits/spins if-else statement
-    /*if ( hits_spins_ratio >= 0.5 && hits<spins) {
-        progress='You win!';
-    } else if( hits_spins_ratio >= 0.25 ) {
-        progress = 'Almost there!';
-    } else if( hits_spins_ratio > 0) {
-        progress='On your way!';
-    } else {
-        progress = 'Get going!' ;
-    }*/
-
-    //ChatGPT's if-elseif-else output
     if (hits_spins_ratio >= 0.5 && hits < spins) {
         progress = 'You win!';
     } else if (hits_spins_ratio >= 0.25) {
@@ -82,14 +110,9 @@ function resetClassName(element){
     } else{
         changeClassName(element);
     }
-    //hits=hits+=2;
     if(spins<2*hits&&hits<spins){
-        //wins=true;
         overhalf=true;
-    } else {
-        //wins=false;
-    }
-    //win_span.innerHTML=wins;
+    } 
     win_span.innerHTML=over_half;
     hits_span.innerHTML = hits; 
     hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
@@ -97,32 +120,6 @@ function resetClassName(element){
     // -- Winning progress depends on hits/spins
     let progress;
     let hits_spins_ratio = hits/spins;
-    /*if ( hits_spins_ratio > 0 ) {
-        progress = 'On your way!';
-        if ( hits_spins_ratio >= 0.25 ) {
-            progress = 'Almost there!';
-            if ( hits_spins_ratio >= 0.5 ) {
-                if( hits < spins) { 
-                    progress = 'You win!';
-                }
-            }
-        }
-    } else {
-        progress = 'Get going!' ;
-    }*/
-
-    //Revamped hits/spins if-else statement
-    /*if ( hits_spins_ratio >= 0.5 && hits<spins) {
-        progress='You win!';
-    } else if( hits_spins_ratio >= 0.25 ) {
-        progress = 'Almost there!';
-    } else if( hits_spins_ratio > 0) {
-        progress='On your way!';
-    } else {
-        progress = 'Get going!' ;
-    }*/
-
-    //ChatGPT's if-elseif-else output
     if (hits_spins_ratio >= 0.5 && hits < spins) {
         progress = 'You win!';
     } else if (hits_spins_ratio >= 0.25) {
