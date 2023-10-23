c97efd72b6675c05a3bf135bec759d843877d8e4
BJRamel <Bramel@hawaii.edu>
2023-10-22 16:29:00 -1000

    xxx

--- Lab9/lab9ex5.js
+++ Lab9/lab9ex5.js
@@ -5,16 +5,16 @@
 
 console.log(junk1());*/
 
-function junk2() {
+/*function junk2() {
     for(var i2=1; i2<=2; i2++){console.log(i2);}
     return `i2 is ${i2}`;
 }
 
-console.log(junk2());
+console.log(junk2());*/
 
-/*function junk3() {
+function junk3() {
     for(let i3=1; i3<=2; i3++){console.log(i3);}
     return `i3 is ${i3}`;
 }
 
-console.log(junk3());*/
+console.log(junk3());
