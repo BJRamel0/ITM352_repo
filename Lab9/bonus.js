df610c6893e5778926e2f846a478627a37228d68
BJRamel <Bramel@hawaii.edu>
2023-10-22 17:14:50 -1000

    xxx

--- Lab9/bonus.js
+++ Lab9/bonus.js
@@ -1,15 +1,23 @@
-/*function NAME (monthly_sales, tax_rate) {
+/*
+psudocode --
+function NAME (monthly_sales, tax_rate) {
+    const tax_owing = [array for one entry for every entry in monthly_sales. Tax owing for entry.]
     return tax_owing 
-};*/
+};
 
-//tax_owing = array for one entry for every entry in monthly_sales. Tax owing for entry.
-//let monthly_sales = [list of montly sales amount];
-//const tax_rate = NUMBER;
 
+let monthly_sales = [list of montly sales amount];
+const tax_rate = NUMBER;
+
+console.log(NAME(parameters));
+*/
+
+//define variables
 const monthly_sales = [10000, 9000, 11000, 12000, 13000];
 const tax_rate = 0.0444; //Hawaii's average combined state and local sales tax rate
-const tax_owing = calculateTax(monthly_sales, tax_rate);
 
+/*calculateTax loops to take the monthly_sales and tax_rate for each value in the array
+and multiplies them to obtain and return a tax_owing array.*/
 function calculateTax (monthly_sales, tax_rate) {
     const tax_owing = [];
     for (let i = 0; i < monthly_sales.length; i++) {
@@ -19,4 +27,5 @@ function calculateTax (monthly_sales, tax_rate) {
     return tax_owing;
 };
 
-console.log(tax_owing);
+//print tax_owing onto console
+console.log(calculateTax(monthly_sales, tax_rate));
