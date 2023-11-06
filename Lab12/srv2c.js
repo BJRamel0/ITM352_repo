1304224773b7d24c3e2f72e8aaaeb4d3c7f03acf
BJRamel <Bramel@hawaii.edu>
2023-11-05 23:27:42 -1000

    xxx

---
+++ Lab12/srv2c.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+//part2a
+app.get('/test', function (req, res) {
+    res.send("<h2>Testing Page</h2>");
+});
+
+app.all('*', function (request, response, next) {
+    //response.send(request.method + 'to path' + request.path);
+    console.log(request.method + 'to path' + request.path);
+    next();
+});
+
+app.listen (8080, () => console.log('listening on port 8080')); //note the use of an anonymous function here to do a callback
